09232df7380b5f356a6f7bac66b3f5ca748eefb3
Griffin Minster <griffinminster@gmail.com>
2022-01-06 13:51:41 -0600

    Factor out basic computations to functions

--- collatz_script.py
+++ collatz_script.py
@@ -1,4 +1,6 @@
 import locale
+import sys
+
 import plotly.express as px
 from tqdm import tqdm
 
@@ -19,12 +21,13 @@ def main():
 
 def range_option():
     #Parse inputs
-    num_range = int(input("What number do you want to test up to? "))
+    num_range = -1
+    while num_range <= 0:
+        num_range = int(input("What number do you want to test up to? "))
     input_arr = list(range(1, num_range+1))
 
     overall_max = (-1, -1)
     overall_tst = (-1, -1)
-
 
     max_x = []
     max_y = []
@@ -35,8 +38,8 @@ def range_option():
     for elem in tqdm(input_arr):
         num = elem
         highest = elem
-        COUNTER = 0
-        curr_x = [COUNTER]
+        counter = 0
+        curr_x = [counter]
         curr_y = [num]
 
         #Performs main 3n+1 or n/2 computation until 1 is reached, ending the loop
@@ -46,32 +49,25 @@ def range_option():
             else:
                 num = num * 3 + 1
             highest = max(num, highest)
-            COUNTER += 1
+            counter += 1
             
-            curr_x.append(COUNTER)
+            curr_x.append(counter)
             curr_y.append(num)
 
         #Prints results for each number, only if inital number was 100 or less
         if num_range <= 100:
-            print(f"Starting Number: {elem}")
-            print(f"Highest peak: {highest}")
-            print(f"Numbers gone through: {COUNTER+1}")
-            print()
+            print_each_result(elem, highest, counter)
 
         #Figure out overall results for max number reached and most numbers gone through each loop pass
         if highest >= overall_max[1]:
             overall_max = (elem, highest)
-            max_x.clear()
-            max_y.clear()
-            max_x = curr_x[:]
-            max_y = curr_y[:]
+            max_x = array_copy(curr_x, curr_y, max_x, max_y)[0]
+            max_y = array_copy(curr_x, curr_y, max_x, max_y)[1]
         
-        if COUNTER >= overall_tst[1]:
-            overall_tst = (elem, COUNTER+1)
-            most_x.clear()
-            most_y.clear()
-            most_x = curr_x[:]
-            most_y = curr_y[:]
+        if counter >= overall_tst[1]:
+            overall_tst = (elem, counter+1)
+            most_x = array_copy(curr_x, curr_y, most_x, most_y)[0]
+            most_y = array_copy(curr_x, curr_y, most_x, most_y)[1]
             
         curr_x.clear()
         curr_y.clear()
@@ -81,21 +77,27 @@ def range_option():
     print(f"Highest Number Reached: {overall_max[1]:n} from #{overall_max[0]:n}")
     print(f"Largest Total Stopping Time: {overall_tst[1]:n} from #{overall_tst[0]:n}")
 
-    fig1 = px.line(x = max_x, y = max_y, title = "Highest Number Reached: From " + str(overall_max[0]), 
-                labels={'x':"Number Hit", 'y':"Result of Computation"})
-    fig2 = px.line(x = most_x, y = most_y, title = "Largest Total Stopping Time: From " + str(overall_tst[0]), 
-                labels={'x':"Numbers Hit", 'y':"Result of Computation"})
+
+    fig1 = px.line(x = max_x, y = max_y, 
+                    title = "Highest Number Reached: From " + str(overall_max[0]), 
+                    labels={'x':"Number Hit", 'y':"Result of Computation"})
+    fig2 = px.line(x = most_x, y = most_y, 
+                    title = "Largest Total Stopping Time: From " + str(overall_tst[0]), 
+                    labels={'x':"Numbers Hit", 'y':"Result of Computation"})
     fig1.show()
     fig2.show()
     
-    exit()
+    sys.exit()
     
     
 def number_option():
-    num = int(input("What number do you want to test? "))
+    num = -1
+    while num <= 0:
+        num = int(input("What number do you want to test? "))
+    starter = num
     highest = num
-    COUNTER = 0
-    max_x = [COUNTER]
+    counter = 0
+    max_x = [counter]
     max_y = [num]
     
     #Performs main 3n+1 or n/2 computation until 1 is reached, ending the loop
@@ -105,22 +107,34 @@ def number_option():
         else:
             num = num * 3 + 1
         highest = max(num, highest)
-        COUNTER += 1
+        counter += 1
         
-        max_x.append(COUNTER)
+        max_x.append(counter)
         max_y.append(num)
     
     #Prints results
-    print(f"Starting Number: {num}")
-    print(f"Highest peak: {highest}")
-    print(f"Numbers gone through: {COUNTER+1}")
-    print()
+    print_each_result(starter, highest, counter)
     
-    fig3 = px.line(x = max_x, y = max_y, title = "Number: " + str(num), 
-                   labels={'x':"Numbers Hit", 'y':"Result of Computation"})
+    fig3 = px.line(x = max_x, y = max_y, 
+                    title = "Number: " + str(starter), 
+                    labels={'x':"Numbers Hit", 'y':"Result of Computation"})
     fig3.show()
     
-    exit()
+    sys.exit()
+    
+    
+def print_each_result(number, highest, counter):
+    print(f"Starting Number: {number}")
+    print(f"Highest peak: {highest}")
+    print(f"Numbers gone through: {counter+1}")
+    print()
+
+def array_copy(curr_x, curr_y, list_x, list_y):
+    list_x.clear()
+    list_y.clear()
+    list_x = curr_x[:]
+    list_y = curr_y[:]
+    return (list_x, list_y)
 
 
 if __name__ == "__main__":
